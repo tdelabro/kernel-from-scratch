af473a22c4272a10aa7a3dcc3642db0149fa6cac
tdelabro <tdelabro@student.42.fr>
2020-12-08 20:12:33 +0100

    dump sgdt fixed

--- src/debug.rs
+++ src/debug.rs
@@ -28,9 +28,8 @@ pub fn dump_segment_registers() {
     let fs: u16;
     let gs: u16;
 
-    let x = gdtr as *const _;
     unsafe {
-        asm!("sgdt ({})", out(reg) x, options(att_syntax));
+        asm!("sgdt [{}]", in(reg) &mut gdtr as *const _);
         asm!("mov ax, cs", out("ax") cs);
         asm!("mov ax, ds", out("ax") ds);
         asm!("mov ax, ss", out("ax") ss);
@@ -38,20 +37,9 @@ pub fn dump_segment_registers() {
         asm!("mov ax, fs", out("ax") fs);
         asm!("mov ax, gs", out("ax") gs);
     }
-    println!("gdtr {:?}", gdtr);
-
-    /*
     unsafe {
-        let gdtr: &GdtR = match (r_gdtr as *const GdtR).as_ref() {
-            Some(p) => p,
-            None => {
-                println!("Error: gdtr point to null");
-                return;
-            }
-        };
         println!("GDTR\nlimit: {:#06x}, base: {:#010x}\n", gdtr.limit, gdtr.base);
     }
-    */
     println!("Segment Registers\ncs: {:#04x}, ds: {:#04x}, ss: {:#04x}\nes: {:#04x}, fs: {:#04x}, gs: {:#04x}\n",
         cs, ds, ss, es, fs, gs);
 }
